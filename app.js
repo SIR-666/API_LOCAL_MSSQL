24aed09d9023ffcb3b3fb5ab20b8acdddb427e28
root <root@sir666.us-central1-c.c.graphite-plane-432006-s5.internal>
2024-08-13 02:09:17 +0000

    add cros

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const koneksi = require('./config/database');
 const app = express();
+
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 // set body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
